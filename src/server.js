ebd6b193e804225d650c3a9b44712d4a457e5b53
Filipe <filipediascontato@gmail.com>
2021-09-22 13:21:21 -0300

    Fix swagger json import

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const app = express();
 
-app.use("/", swaggerUi.serve, swaggerUi.setup("./swagger.json"));
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(3333, () => console.log("Server is running in http://localhost:3333..."));
