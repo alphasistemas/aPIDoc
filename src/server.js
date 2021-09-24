f8b13fe279e237d4989cd1942ab83a4a2bc91633
Filipe <filipediascontato@gmail.com>
2021-09-24 10:17:30 -0300

    Decouples port enviroment

--- src/server.js
+++ src/server.js
@@ -1,9 +1,21 @@
+require('dotenv').config()
+
 const express = require("express");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
+
+const port = process.env.PORT;
 
 const app = express();
 
 app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.listen(3333, () => console.log("Server is running in http://localhost:3333..."));
+try {
+    if(!process.env.PORT)
+        throw new Error("Host port not found.\nYou must set up a port in env variable!")
+    
+    app.listen(port, () => console.log("Server is running in http://localhost:" + port));
+} catch(error) {
+    console.log(error.message);
+}
+
