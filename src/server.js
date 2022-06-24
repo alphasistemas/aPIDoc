afabcdbdac8ded08ada4d687cb339e9192fd24f7
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2022-06-24 14:22:59 -0300

    update doc

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 try {
     if(!process.env.PORT)
         throw new Error("Host port not found.\nYou must set up a port in env variable!");
-    
+
     app.listen(port, () => console.log("Server is running in http://localhost:" + port));
 } catch(error) {
     console.log(error.message);
